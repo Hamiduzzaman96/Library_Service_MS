357fb14d87cc23464a7dc4f12dfbff65b0c57f09
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2025-12-31 02:36:22 +0600

    mysql repository created for book

--- internal/repository/book_repository.go
+++ internal/repository/book_repository.go
@@ -1,8 +1,12 @@
 package repository
 
-import "github.com/Hamiduzzaman96/Library_Service_MS/internal/domain"
+import (
+	"context"
+
+	"github.com/Hamiduzzaman96/Library_Service_MS/internal/domain"
+)
 
 type BookRepository interface {
-	Create(book *domain.Book) (int64, error)
-	GetByID(id int64) (*domain.Book, error)
+	Create(ctx context.Context, book *domain.Book) error
+	GetByID(ctx context.Context, id int64) (*domain.Book, error)
 }
---
+++ internal/repository/mysql/book_mysql_repository.go
@@ -0,0 +1,42 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/Hamiduzzaman96/Library_Service_MS/internal/domain"
+)
+
+type bookMySQLRepository struct {
+	db *sql.DB
+}
+
+func newBookmySQLRepository(db *sql.DB) *bookMySQLRepository {
+	return &bookMySQLRepository{db: db}
+}
+
+func (r *bookMySQLRepository) Create(ctx context.Context, book *domain.Book) error {
+	query := `INSERT INTO books (title, author) VALUES (?,?)`
+
+	result, err := r.db.ExecContext(ctx, query, book.Title, book.Author)
+
+	if err != nil {
+		return err
+	}
+	id, _ := result.LastInsertId()
+	book.ID = id
+	return nil
+}
+
+func (r *bookMySQLRepository) GetByID(ctx context.Context, id int64) (*domain.Book, error) {
+	query := `SELECT id,title, author FROM books WHERE id = ?`
+	book := &domain.Book{}
+	err := r.db.QueryRowContext(ctx, query, id).Scan(
+		&book.ID, &book.Title, &book.Author,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return book, nil
+
+}
