b74a73832a3126de9e88eb6a6cc101675cc84bc8
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-01 18:42:29 +0600

    main.go created for user

---
+++ cmd/user/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"net"
+	"net/http"
+	"runtime"
+
+	"github.com/Hamiduzzaman96/Library_Service_MS/internal/handler/grpct"
+	"github.com/Hamiduzzaman96/Library_Service_MS/internal/repository/mysql"
+	"github.com/Hamiduzzaman96/Library_Service_MS/internal/usecase"
+	"github.com/Hamiduzzaman96/Library_Service_MS/proto/userpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	//connect to MySQL
+	db, err := sql.Open("mysql", "root:123456 @tcp(localhost:3306)/library")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("failed to connect databse: %v", err)
+	}
+	defer db.Close()
+
+	repo := mysql.NewUserMySQLRepository(db)
+	u := usecase.NewUserUsecase(repo)
+
+	// grpc server
+	handler := grpct.NewUserHandler(u)
+	lis, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	userpb.RegisterUserServiceServer(grpcServer, handler)
+
+	go func() {
+		log.Println("gRPC user service running on :50052")
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	//HTTP client-facing (gRPC to HTTP)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	err = userpb.RegisterUserServiceHandlerFromEndpoint(
+		ctx,
+		mux,
+		"localhost:50052",
+		[]grpc.DialOption{grpc.WithInsecure()},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("HTTP User API running on :8081")
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		log.Fatal(err)
+	}
+
+}
