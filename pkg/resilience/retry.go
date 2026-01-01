0436021fefb7d7cd10429df29c019aefe43875b8
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-01 18:56:07 +0600

    retry.go created

---
+++ pkg/resilience/retry.go
@@ -0,0 +1,14 @@
+package resilience
+
+import "time"
+
+func Retry(attempts int, delay time.Duration, fn func() error) error {
+	var err error
+	for i := 0; i < attempts; i++ {
+		if err = fn(); err == nil {
+			return nil
+		}
+		time.Sleep(delay)
+	}
+	return err
+}
