2fe00f45d7d2b388a6662b57ea3753fb78173809
Hamiduzzaman96 <hamiduzzaman.ramim96@gmail.com>
2026-01-03 01:46:24 +0600

    updated user main.go

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,28 +1,36 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"log"
 	"net"
-	"net/http"
 
 	"github.com/Hamiduzzaman96/Library_Service_MS/internal/handler/grpct"
 	"github.com/Hamiduzzaman96/Library_Service_MS/internal/repository/mysql"
 	"github.com/Hamiduzzaman96/Library_Service_MS/internal/usecase"
+	"github.com/Hamiduzzaman96/Library_Service_MS/pkg/config"
 	"github.com/Hamiduzzaman96/Library_Service_MS/proto/userpb"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	//connect to MySQL
-	db, err := sql.Open("mysql", "root:ramim#rimi96 @tcp(localhost:3306)/library")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// db, err := sql.Open("mysql", "root:pass @tcp(localhost:3306)/library")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
+	// if err := db.Ping(); err != nil {
+	// 	log.Fatalf("failed to connect databse: %v", err)
+	// }
+	// defer db.Close()
+
+	cfg := config.LoadUserConfig()
+
+	db, err := sql.Open("mysql", cfg.DBDSN)
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
 	if err := db.Ping(); err != nil {
 		log.Fatalf("failed to connect databse: %v", err)
 	}
@@ -33,41 +41,18 @@ func main() {
 
 	// grpc server
 	handler := grpct.NewUserHandler(u)
-	lis, err := net.Listen("tcp", ":50052")
+	lis, err := net.Listen("tcp", cfg.GRPCPort)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to listen gRPC : %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
 	userpb.RegisterUserServiceServer(grpcServer, handler)
 
 	go func() {
-		log.Println("gRPC user service running on :50052")
+		log.Println("gRPC user service running on: %s", cfg.GRPCPort)
 		if err := grpcServer.Serve(lis); err != nil {
-			log.Fatal(err)
+			log.Fatalf("gRPC server error :%v", err)
 		}
 	}()
-
-	//HTTP client-facing (gRPC to HTTP)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	mux := runtime.NewServeMux()
-
-	err = userpb.RegisterUserServiceHandlerFromEndpoint(
-		ctx,
-		mux,
-		"localhost:50052",
-		[]grpc.DialOption{grpc.WithInsecure()},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("HTTP User API running on :8081")
-	if err := http.ListenAndServe(":8081", mux); err != nil {
-		log.Fatal(err)
-	}
-
 }
